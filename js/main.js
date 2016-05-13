c91db05e5ddd0e71fa4786dc24513a871cd0735b
Dan Putnam <daniel.a.putnam@gmail.com>
2016-05-12 22:46:02 -0500

    added hamburger menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//$('.main-nav').addClass('open');
+// listen for user click on icon
+$('.hamburger').on('click', function() {
+	//if closed, slide down
+	$('.main-nav').toggleClass('open');
+	//if open, slide up
+
+});	
